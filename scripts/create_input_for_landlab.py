db71fc07ed25cd6bc5290cb5eb9fed407f129110
Christian Werner <chr.werner@me.com>
2018-09-24 10:52:26 +0200

    Add lpj output conversion to create_input_for_landlab.py, needs checking

--- scripts/create_input_for_landlab.py
+++ scripts/create_input_for_landlab.py
@@ -1,23 +1,62 @@
 import numpy as np
 import xarray as xr
+import pandas as pd
 
 """
 set of scripts which makes post-processed lpj-output landlab compatible
 """
 
-def read_csv_files(filename):
+def _calc_fpc(lai):
+    """Calculate FPC using the LPJ-GUESS method
     """
-    reads in the csv files from lpj
+    return (1.0 - np.exp(-0.5 * lai)) * 100
+
+
+def read_csv_files(filename, ftype='lai', pft_class='total'):
+    """
+    reads in the out files from lpj and convertes to aggregated values
 
     resulting 2D-arrays have format:
         
         tree_fpc[0] = landform__ID
         tree_fpc[1] = according vegetation cover
     """
-    
-    fpc_array = np.genfromtxt(filename, delimiter = ';', names = True)
 
-    return fpc_array
+    if ftype != 'lai':
+        raise NotImplementedError
+
+    # these are custom column names (can be configures in LPJ ins file!)
+    index_cols = ['Lat', 'Lon', 'Year', 'Stand', 'Patch'] 
+    tree_cols = ['TeBE_tm','TeBE_itm','TeBE_itscl','TeBS_itm','TeNE','BBS_itm','BBE_itm']
+    shrub_cols = ['BE_s','TeR_s','TeE_s']
+    grass_cols = ['C3G']
+    total_col = ['Total']
+
+    if pft_class == 'total':
+        requested_cols = total_col
+    elif pft_class == 'grass':
+        requested_cols = grass_cols
+    elif pft_class == 'shrub':
+        requested_cols = shrub_cols
+    elif pft_class == 'tree':
+        requested_cols = tree_cols
+    else:
+        raise NotImplementedError
+
+    df = pd.read_table(filename, delim_whitespace=True)[index_cols + requested_cols]
+    df = df[df.Stand > 0]
+    del df['Patch']
+    df_grp = df.groupby(['Lon', 'Lat', 'Year', 'Stand']).mean()
+    df_grp = df_grp.apply(_calc_fpc, 1).sum(axis=1)
+    df = df_grp[v].reset_index().set_index(['Year', 'Stand'])
+    del x['Lon'], x['Lat']
+    fpc_array = x.mean(level=1).T
+    # fpc_array.to_csv(f'fpc_{v}.csv', index=False)
+    #fpc_array = np.genfromtxt(filename, delimiter = ';', names = True)
+
+    # TODO: check if the values() conversion gives what we expect
+
+    return fpc_array.values()
 
 def map_fpc_per_landform_on_grid(grid, fpc_array):
     """
